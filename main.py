1e170dc4980124cd06346cc0e5e1a5bb4bd02a3a
Woland <tkasparaitis@gmail.com>
2024-04-07 07:37:48 -0400

    Able to loop through multiple pages & scrape listings to a dictionary. There's a function for writing the dictionary to csv, but it is not used yet. Next, need to take the csv and write to a database.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import configparser
+import csv
 import os
 import math
 import time
 import random
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
@@ -11,6 +13,86 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+
+
+# NOTE: explain what this does also in comments later
+def is_scroll_at_bottom(scrollable_element):
+    return driver.execute_script("return arguments[0].scrollTop == "
+                                 "(arguments[0].scrollHeight - arguments[0].offsetHeight);", scrollable_element)
+
+
+def scroll_to_end(scrollable_element):
+    while not is_scroll_at_bottom(scrollable_element):
+        # NOTE: explain what this does in comments later
+        # scrollBy(width, height) -> scrolls by those amounts, here we set width to 0
+        driver.execute_script("arguments[0].scrollBy(0, arguments[0].offsetHeight);", scrollable_element)
+        time.sleep(random.uniform(1, 2))
+
+
+# NOTE: Due to recursion, resource usage can be an issue from multiple instances of the same function before completion
+def scrape_job_cards(list_of_elements, job_cards_processed, pages_processed, total_pages):
+    for element in list_of_elements:
+        try:
+            data_job_id = element.get_attribute("data-job-id")
+            job_link = "https://www.linkedin.com/jobs/view/" + data_job_id
+            # Aria label is an accessibility label which seems to have the job title (a more reliable grab):
+            aria_label = element.find_element(By.CSS_SELECTOR, "[aria-label]")
+            job_title = aria_label.get_attribute("aria-label")
+            job_company = element.find_element(
+                By.XPATH, ".//span[contains(@class, 'job-card-container__primary-description')]").text
+            scraped_job_listings[data_job_id] = {
+                'link': job_link,
+                'title': job_title,
+                'company': job_company
+            }
+            job_cards_processed += 1
+            if job_cards_processed % 25 == 0:
+                pages_processed += 1
+                if pages_processed == total_pages:
+                    return
+                else:
+                    time.sleep(random.uniform(3, 6))
+                    next_page_button = driver.find_element(By.XPATH,
+                                                           f"//button[@aria-label='Page {pages_processed + 1}']")
+                    next_page_button.click()
+                    time.sleep(random.uniform(5, 10))
+                    scroll_to_end(driver.find_element(By.CLASS_NAME, "jobs-search-results-list"))
+                    scrape_job_cards(driver.find_elements(By.CSS_SELECTOR, "[data-job-id]"),
+                                     job_cards_processed, pages_processed, total_pages)
+        except NoSuchElementException:
+            pass
+
+
+def get_total_pages():
+    listings_element = wait.until(ec.element_to_be_clickable(
+        (By.XPATH, "//div[@class='jobs-search-results-list__subtitle']"))
+    )
+    listings_text = listings_element.text
+    total_listings_str = listings_text.strip().split()[0]  # Get the first word (our number of results)
+    total_listings_str = total_listings_str.replace(',', '')  # Remove comma(s) in for example 1,000
+    total_listings = int(total_listings_str)
+    listings_per_page = 25
+    return math.ceil(total_listings / listings_per_page)
+
+
+def write_to_csv(job_listings):
+    output_dir = 'job_listings_data'
+    os.makedirs(output_dir, exist_ok=True)
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    filename = f'job_listings_{timestamp}.csv'
+    filepath = os.path.join(output_dir, filename)
+    with open(filepath, 'w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = ['job_id', 'job_title', 'job_company', 'job_link']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for job_id, listing in job_listings.items():
+            writer.writerow({
+                'job_id': job_id,
+                'job_title': listing['job_title'],
+                'job_company': listing['job_company'],
+                'job_link': listing['job_link']
+            })
+
 
 config_file_path = os.path.join(os.path.join(os.environ['USERPROFILE'], 'Desktop'), 'config.ini')
 config = configparser.ConfigParser()
@@ -59,9 +141,12 @@ keyword_box = wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains
 search_one_keywords = config['searchOne']['keywords']
 search_one_location = config['searchOne']['location']
 keyword_box.send_keys(search_one_keywords)
+time.sleep(random.uniform(1, 2))
 location_box = wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'jobs-search-box-location')]")))
 location_box.send_keys(search_one_location)
+time.sleep(random.uniform(1, 2))
 keyword_box.send_keys(Keys.ENTER)
+time.sleep(random.uniform(1, 2))
 
 # Clear out any filters if there are any from previous uses or being signed in to LinkedIn etc.:
 try:
@@ -77,67 +162,12 @@ except TimeoutException:
     pass
 
 # TODO: apply filter for remote for SearchOne; add DatePosted as a measure of system time - what they have on listing
+# TODO: think about some search options since LI/Indeed search isn't great - for example developer I, SDE I etc.
 
-# Get the # of results & # of pages that we will iterate through:
-listings_element = wait.until(ec.element_to_be_clickable(
-    (By.XPATH, "//div[@class='jobs-search-results-list__subtitle']"))
-)
-listings_text = listings_element.text
-total_listings_str = listings_text.strip().split()[0]  # Get the first word (our number of results)
-total_listings_str = total_listings_str.replace(',', '')  # Remove comma(s) in for example 1,000
-total_listings = int(total_listings_str)
-listings_per_page = 25
-total_pages = math.ceil(total_listings / listings_per_page)
-
-# Start iterating over the job cards:
-processed_job_cards = 0
-processed_pages = 0
-
-
-def is_scroll_at_bottom(scrollable_element):
-    return driver.execute_script("return arguments[0].scrollTop == "
-                                 "(arguments[0].scrollHeight - arguments[0].offsetHeight);", scrollable_element)
-
-
-def scroll_to_end(scrollable_element):
-    while not is_scroll_at_bottom(scrollable_element):
-        # scrollBy(width, height) -> scrolls by those amounts, here we set width to 0
-        #
-        driver.execute_script("arguments[0].scrollBy(0, arguments[0].offsetHeight);", scrollable_element)
-        time.sleep(random.uniform(1, 2))
-
-
-def scrape_job_cards(list_of_elements, cards, pages):
-    for element in list_of_elements:
-        try:
-            data_job_id = element.get_attribute("data-job-id")
-            job_link = "https://www.linkedin.com/jobs/view/" + data_job_id
-            # Aria label is an accessibility label which seems to have the job title (a more reliable grab):
-            aria_label = element.find_element(By.CSS_SELECTOR, "[aria-label]")
-            job_title = aria_label.get_attribute("aria-label")
-            job_company = element.find_element(
-                By.XPATH, ".//span[contains(@class, 'job-card-container__primary-description')]").text
-            print(data_job_id + " | " + job_link + " | " + job_title + " | " + job_company)
-            cards += 1
-            if cards % 25 == 0:
-                pages += 1
-                if pages == total_pages:
-                    break
-                else:
-                    time.sleep(random.uniform(3, 6))
-                    next_page_button = driver.find_element(By.XPATH,
-                                                           f"//button[@aria-label='Page {pages + 1}']")
-                    next_page_button.click()
-        except NoSuchElementException:
-            pass
-
-
-jobs_search_results_element = driver.find_element(By.CLASS_NAME, "jobs-search-results-list")
-scroll_to_end(jobs_search_results_element)
-elements = driver.find_elements(By.CSS_SELECTOR, "[data-job-id]")
-
-# NOTE: probably need to put this into a method so it can call itself at the end of a run
-
+scraped_job_listings = {}
+scroll_to_end(driver.find_element(By.CLASS_NAME, "jobs-search-results-list"))
+scrape_job_cards(driver.find_elements(By.CSS_SELECTOR, "[data-job-id]"),
+                 0, 0, get_total_pages())
 
 # TODO: maybe think about an indefinite amount of searches, how to read the config.ini in that case & iterate
 # TODO: idea - feed jobs found to ChatGPT to filter out things I don't want
