973f9343f687e998bd83dd3d1b83251303c5f52e
Woland <tkasparaitis@gmail.com>
2024-04-15 17:58:32 -0400

    Refactored more code into functions; Added PySpark to read from CSV files and write to a PostgreSQL database.

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
 
 # NOTE: explain what this does also in comments later
@@ -33,7 +34,7 @@ def scrape_job_cards(list_of_elements):
         for element in list_of_elements:
             linked_in_id = element.get_attribute("data-job-id")
             link = "https://www.linkedin.com/jobs/view/" + linked_in_id
-            # FIXME: something wrong here with the way title & company are being grabbed that causes a crash on first page
+            # FIXME: something wrong with the way title & company are being grabbed that causes a crash on first page
             aria_label = element.find_element(By.CSS_SELECTOR, 'a.job-card-container__link[aria-label][tabindex="0"]')
             title = aria_label.get_attribute("aria-label")
             # FIXME: maybe change the code below to get attribute after locating element?
@@ -49,7 +50,11 @@ def scrape_job_cards(list_of_elements):
 
 
 def get_next_page():
-    current_page_button = driver.find_element(By.XPATH, "//button[@aria-current='true']")
+    # When there is only 1 page of results, LinkedIn does not display a number at the bottom of the page
+    try:
+        current_page_button = driver.find_element(By.XPATH, "//button[@aria-current='true']")
+    except NoSuchElementException:
+        return None
     current_page_label = current_page_button.get_attribute("aria-label")
     # Though LinkedIn can show "..." instead of a page number, the aria-label will always have the Page # due to ADA
     next_page_number = int(current_page_label.split()[1]) + 1
@@ -60,12 +65,7 @@ def get_next_page():
         return None
 
 
-def write_to_csv(job_listings):
-    output_dir = os.path.join(os.path.join(os.environ['USERPROFILE'], 'Desktop'), 'JobScraper')
-    os.makedirs(output_dir, exist_ok=True)
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M')
-    filename = f'LinkedIn_{timestamp}.csv'
-    filepath = os.path.join(output_dir, filename)
+def write_to_csv(job_listings, filepath):
     with open(filepath, 'w', newline='', encoding='utf-8') as csv_file:
         fieldnames = ['linked_in_id', 'title', 'company', 'link']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -77,6 +77,7 @@ def write_to_csv(job_listings):
                 'company': listing['company'],
                 'link': listing['link']
             })
+    return filepath
 
 
 def scrape_job_pages():
@@ -119,11 +120,88 @@ def apply_job_filters():
     show_results.click()
 
 
-config_file_path = os.path.join(os.path.join(os.environ['USERPROFILE'], 'Desktop'), 'config.ini')
-config = configparser.ConfigParser()
-config.read(config_file_path)
-username = config['credentials']['username']
-password = config['credentials']['password']
+def write_to_database(filepath):
+    config = read_config_file()
+    spark = SparkSession.builder.appName("LinkedIn Listing Loader").getOrCreate()
+    host = config['database']['host']
+    port = config['database']['port']
+    database = config['database']['database']
+    connection_url = f"jdbc:postgresql://{host}:{port}/{database}"
+    connection_properties = {
+        "user": config['database']['username'],
+        "password": config['database']['password'],
+        "driver": "org.postgresql.Driver"
+    }
+    # NOTE: once more of this is fleshed out, change table name to not be hardcoded
+    spark.read.jdbc(url=connection_url, table="linkedin", properties=connection_properties)
+    dataframe = spark.read.csv(filepath, header=True)
+    dataframe = dataframe.filter(~col('linked_in_id').rlike('[^0-9]'))
+    # TODO: Need to account for ID collisions, we don't want to overwrite existing IDs
+    dataframe.write.jdbc(url=connection_url, table="linkedin", mode="append", properties=connection_properties)
+
+
+def read_config_file():
+    config_file_path = os.path.join(os.path.join(os.environ['USERPROFILE'], 'Desktop'), 'config.ini')
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    return config
+
+
+def generate_filepath():
+    output_dir = os.path.join(os.path.join(os.environ['USERPROFILE'], 'Desktop'), 'JobScraper')
+    os.makedirs(output_dir, exist_ok=True)
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M')
+    filename = f'LinkedIn_{timestamp}.csv'
+    filepath = os.path.join(output_dir, filename)
+    return filepath
+
+
+def sign_in():
+    config = read_config_file()
+    username = config['credentials']['username']
+    password = config['credentials']['password']
+    username_box = wait.until(ec.element_to_be_clickable((By.ID, "session_key")))
+    username_box.send_keys(username)
+    username_box = wait.until(ec.element_to_be_clickable((By.ID, "session_password")))
+    username_box.send_keys(password)
+    sign_in_button = wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@data-id='sign-in-form__submit-btn']")))
+    sign_in_button.click()
+    # Wait to check that we are on the homepage:
+    wait.until(ec.title_contains("Feed | LinkedIn"))
+
+
+def minimize_message_window():
+    # Minimize messaging for better view when testing:
+    # FIXME: fix whatever is causing the crash here:
+    minimize_chevron = wait.until(ec.element_to_be_clickable(
+        (By.XPATH, "//div[contains(@class, 'msg-overlay-bubble-header__controls')]//use[@href='#chevron-down-small']"))
+    )
+    minimize_chevron.click()
+
+
+def navigate_to_jobs():
+    jobs_page = wait.until(ec.element_to_be_clickable(
+        (By.XPATH, "//a[contains(@class, 'app-aware-link') and @href='https://www.linkedin.com/jobs/?']"))
+    )
+    jobs_page.click()
+    wait.until(ec.title_contains("Jobs | LinkedIn"))
+
+
+def input_search_keywords():
+    config = read_config_file()
+    keyword_box = wait.until(
+        ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'jobs-search-box-keyword')]")))
+    search_one_keywords = config['searchOne']['keywords']
+    search_one_location = config['searchOne']['location']
+    keyword_box.send_keys(search_one_keywords)
+    time.sleep(random.uniform(1, 2))
+    location_box = wait.until(
+        ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'jobs-search-box-location')]")))
+    location_box.send_keys(search_one_location)
+    time.sleep(random.uniform(1, 2))
+    keyword_box.send_keys(Keys.ENTER)
+    time.sleep(random.uniform(1, 2))
+
 
 # NOTE: re-enable headless mode after this is fleshed out
 # from selenium.webdriver.chrome.options import Options
@@ -133,56 +211,26 @@ password = config['credentials']['password']
 
 # driver = webdriver.Chrome(options=options)
 
-# Uses credentials from config file to sign in to LinkedIn
 # TODO: Account for auto-sign in from Google etc., probably check for page title?
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.linkedin.com')
 wait = WebDriverWait(driver, 10)
-actions = ActionChains(driver)
-username_box = wait.until(ec.element_to_be_clickable((By.ID, "session_key")))
-username_box.send_keys(username)
-username_box = wait.until(ec.element_to_be_clickable((By.ID, "session_password")))
-username_box.send_keys(password)
-sign_in_button = wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@data-id='sign-in-form__submit-btn']")))
-sign_in_button.click()
-
-# Wait to check that we are on the homepage:
-wait.until(ec.title_contains("Feed | LinkedIn"))
-# Minimize messaging for better view when testing:
-# FIXME: fix whatever is causing the crash here:
-# messaging_minimize_button = wait.until(EC.element_to_be_clickable(
-#     (By.XPATH, "//div[contains(@class, 'msg-overlay-bubble-header__controls')]//use[@href='#chevron-down-small']"))
-# )
-# messaging_minimize_button.click()
-
-# Go to jobs page
-jobs_page = wait.until(ec.element_to_be_clickable(
-    (By.XPATH, "//a[contains(@class, 'app-aware-link') and @href='https://www.linkedin.com/jobs/?']"))
-)
-jobs_page.click()
-wait.until(ec.title_contains("Jobs | LinkedIn"))
-keyword_box = wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'jobs-search-box-keyword')]")))
-search_one_keywords = config['searchOne']['keywords']
-search_one_location = config['searchOne']['location']
-keyword_box.send_keys(search_one_keywords)
-time.sleep(random.uniform(1, 2))
-location_box = wait.until(ec.element_to_be_clickable((By.XPATH, "//input[contains(@id, 'jobs-search-box-location')]")))
-location_box.send_keys(search_one_location)
-time.sleep(random.uniform(1, 2))
-keyword_box.send_keys(Keys.ENTER)
-time.sleep(random.uniform(1, 2))
-
+sign_in()
+navigate_to_jobs()
+input_search_keywords()
 scraped_job_listings = {}
 scrape_job_pages()
-write_to_csv(scraped_job_listings)
+csv_filepath = generate_filepath()
+write_to_csv(scraped_job_listings, csv_filepath)
+write_to_database(csv_filepath)
+input("Press any key in the run window to exit session")  # This pauses the script until the user does something
+driver.quit()  # This closes the browser window and exits the WebDriver session
 
-# TODO: apply filter for remote for SearchOne; add DatePosted as a measure of system time - what they have on listing
+
 # TODO: think about some search options since LI/Indeed search isn't great - for example developer I, SDE I etc.
 # TODO: maybe think about an indefinite amount of searches, how to read the config.ini in that case & iterate
 # TODO: idea - feed jobs found to ChatGPT to filter out things I don't want
 # TODO: check for unique job identifiers (for example Indeed has repeats where jk=# is repeated among different pages)
 # TODO: automate the run for every X hours using scheduler.py along with Windows Task Scheduler
-
-input("Press any key in the run window to exit session")  # This pauses the script until the user does something
-driver.quit()  # This closes the browser window and exits the WebDriver session
